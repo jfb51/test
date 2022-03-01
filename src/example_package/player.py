4cec25256a92f59da1ac79b5cdcabe9f278ecdba
jfb51 <jbennettlongley@gmail.com>
2022-03-01 21:44:40 +1100

    Change player attributes

--- src/example_package/player.py
+++ src/example_package/player.py
@@ -1,24 +1,26 @@
 class Bowler:
-    def __init__(self, pp, historic_career_stats, current_match_stats=None):
+    def __init__(self, name, pp, historic_career_stats, current_match_stats=None):
         bowler_in_game_attributes = ['bowler_extras', 'bowler_runs_b4b', 'bowler_wickets_b4b',
                                      'bowler_balls_bowled_b4b', 'bowler_dots_b4b', 'bowler_er_b4b']
-        self.name = pp['name']
-        self.bowling_style = pp['bowling_style']
-        self.historic_career_stats = historic_career_stats.loc[self.name]
+        self.name = name
+        self.pp = pp
+        self.historic_career_stats = historic_career_stats.loc[self.name].to_dict()
+        self.historic_career_stats['bowling_style'] = self.pp['bowling_style']
         if current_match_stats:
-            self.current_match_stats = current_match_stats[bowler_in_game_attributes].to_dict()
+            self.current_match_stats = current_match_stats[bowler_in_game_attributes]
         else:
             self.current_match_stats = {c: 0 for c in bowler_in_game_attributes}
 
 
 class Batter:
-    def __init__(self, pp, historic_career_stats, current_match_stats=None):
+    def __init__(self, name, pp, historic_career_stats, current_match_stats=None):
         batter_in_game_attributes = ['batting_position', 'striker_runs_b4b', 'striker_balls_faced_b4b',
                                      'strike_rate_b4b']
-        self.name = pp['name']
-        self.batting_style = pp['batting_style']
-        self.historic_career_stats = historic_career_stats.loc[self.name]
+        self.name = name
+        self.pp = pp
+        self.historic_career_stats = historic_career_stats.loc[self.name].to_dict()
+        self.historic_career_stats['batting_style'] = self.pp['batting_style']
         if current_match_stats:
-            self.current_match_stats = current_match_stats[batter_in_game_attributes].to_dict()
+            self.current_match_stats = current_match_stats[batter_in_game_attributes]
         else:
             self.current_match_stats = {c: 0 for c in batter_in_game_attributes}
