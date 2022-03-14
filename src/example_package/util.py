baf17f8b9eba181b5367f6f9af25152a5b50dab3
jfb51 <jbennettlongley@gmail.com>
2022-03-15 10:54:20 +1100

    tweak names

--- src/example_package/util.py
+++ src/example_package/util.py
@@ -3,18 +3,18 @@ from collections import namedtuple, OrderedDict
 from math import erf
 
 
-def kelly_bet(row, comission=0.05, bankroll=1000):
+def kelly_bet(row, commission=0.05, bankroll=1000):
     if row.side == "None":
         return 0
     elif row.side == 'Back':
         d = row.decimal_offer
         p = row.estimated_probability_lower
-        f = (p * (d - 1) * (1 - comission) - (1 - p)) / ((d - 1) * (1 - comission))
+        f = (p * (d - 1) * (1 - commission) - (1 - p)) / ((d - 1) * (1 - commission))
         return bankroll * np.minimum(f, 1)
     else:
         d = row.decimal_bid
         p = row.estimated_probability_upper
-        f = ((1 - p) * (1 - comission) - p * (d - 1)) / ((d - 1) * (1 - comission))
+        f = ((1 - p) * (1 - commission) - p * (d - 1)) / ((d - 1) * (1 - commission))
         return -bankroll * np.minimum(f, 1 / (d - 1))
 
 
