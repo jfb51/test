e95911433abf452d357e1dec0071ad0e1b20ddcb
jfb51 <jbennettlongley@gmail.com>
2022-03-28 09:48:59 +1100

    Update inputs

--- src/example_package/tradingstrategy.py
+++ src/example_package/tradingstrategy.py
@@ -7,8 +7,8 @@ from example_package.util import kelly_bet
 
 
 class TradingStrategy:
-    def __init__(self, match_id, series_id, match_row, historic_match_data, player_info,
-                 wicket_model, runs, wms, nbs, num_sims,
+    def __init__(self, match_id, series_id, match_row, historic_match_data, career_bowling_data, career_batting_data,
+                 wicket_model, runs, wms, nbs, bowling_models, num_sims,
                  from_pickle, nautilus_path, smooth_probs=True):
         self.match_id = match_id
         self.series_id = series_id
@@ -16,13 +16,13 @@ class TradingStrategy:
         self.smooth_probs = smooth_probs
         self.nautilus_path = nautilus_path
         self.from_pickle = from_pickle
-        self.match = HistoricMatchSimulator(match_id, match_row, historic_match_data, player_info,
-                                            wicket_model, runs, wms, nbs)
+        self.match = HistoricMatchSimulator(match_id, match_row, historic_match_data, career_bowling_data,
+                                            career_batting_data, wicket_model, runs, wms, nbs, bowling_models)
         self.bd = BettingData(from_pickle=self.from_pickle, nautilus_path=self.nautilus_path)
         self.betting_data = self.bd.combine_trade_and_market_df()
         self.betting_reference_team = self.bd.team_map[self.bd.first_team_id]
-        if self.betting_reference_team!=self.match.chasing_team:
-            self.negate_sim_probabilities=True
+        if self.betting_reference_team != self.match.chasing_team:
+            self.negate_sim_probabilities = True
 
     def simulate_historic_match(self):
         # these probabilities are in terms of the chasing team - need to make this consistent with the odds information
