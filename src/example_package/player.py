9649261b399e4953c8aa376cbcb43b8dde7fccdb
jfb51 <jbennettlongley@gmail.com>
2022-05-03 21:53:43 +1200

    Fix typo

--- src/example_package/player.py
+++ src/example_package/player.py
@@ -1,8 +1,8 @@
 class Bowler:
     def __init__(self, pp, historic_career_stats):
         self.bowler_in_game_attributes = ['bowler_extras', 'bowler_runs_b4b', 'bowler_wickets_b4b',
-                                          'bowler_balls_bowled_b4b', 'bowler_dots_b4b', 'bowler_er_b4b', 'bowler_prop_0',
-                                          'bowler_prop_1', 'bowler_prop_2', 'bowler_prop_4', 'bowler_prop_6']
+                                          'bowler_balls_bowled_b4b', 'bowler_dots_b4b', 'bowler_er_b4b', 'bowler_0_b4b',
+                                          'bowler_1_b4b', 'bowler_2_b4b', 'bowler_4_b4b', 'bowler_6_b4b']
         self.pp = pp
         self.historic_career_stats = historic_career_stats.loc[self.pp['unique_name']].to_dict(orient='records')[0]
         self.historic_career_stats['bowling_style'] = self.pp['simple_bowling']
@@ -18,8 +18,8 @@ class Bowler:
 class Batter:
     def __init__(self, pp, historic_career_stats):
         self.batter_in_game_attributes = ['batting_position_bat', 'striker_runs_b4b', 'striker_balls_faced_b4b',
-                                          'strike_rate_b4b', 'striker_prop_0', 'striker_prop_1', 'striker_prop_2',
-                                          'striker_prop_4', 'striker_prop_6']
+                                          'strike_rate_b4b', 'striker_0_b4b', 'striker_1_b4b', 'striker_2_b4b',
+                                          'striker_4_b4b', 'striker_6_b4b']
         self.pp = pp
         self.historic_career_stats = historic_career_stats.loc[self.pp['unique_name']].to_dict(orient='records')[0]
         self.historic_career_stats['batting_style'] = self.pp['Batting Style']
